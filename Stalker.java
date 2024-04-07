6a5e49b99ea8ad325e5c3ce3a4ef1bbc9fe92670
Sumit Chauhan <chauhansumit9009@gmail.com>
2024-04-07 19:21:38 +0530

    init

---
+++ Stalker.java
@@ -0,0 +1,9 @@
+/**
+ * Stalker
+ */
+public class Stalker {
+
+    public static void main(String[] args) {
+        System.out.println("Hi Stalker! you came here, do explore other files as well they are a great resource too!");
+    }
+}
